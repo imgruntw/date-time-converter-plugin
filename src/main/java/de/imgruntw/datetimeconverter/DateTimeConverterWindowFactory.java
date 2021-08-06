822af3e0c2862e05dd3dd44da48ddbafa51ba903
Wladislaw Imgrunt <w.imgrunt@itsonix.eu>
2021-08-06 11:00:18 +0200

    Release 1.10

--- src/main/java/de/imgruntw/datetimeconverter/DateTimeConverterWindowFactory.java
+++ src/main/java/de/imgruntw/datetimeconverter/DateTimeConverterWindowFactory.java
@@ -1,18 +1,20 @@
 package de.imgruntw.datetimeconverter;
 
-import com.intellij.openapi.components.ServiceManager;
+import org.jetbrains.annotations.NotNull;
+
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowFactory;
 import com.intellij.ui.content.Content;
 import com.intellij.ui.content.ContentFactory;
-import org.jetbrains.annotations.NotNull;
 
 public final class DateTimeConverterWindowFactory implements ToolWindowFactory {
 
     @Override
     public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
-        final DateTimeConverterViewService viewService = ServiceManager.getService(DateTimeConverterViewService.class);
+        final DateTimeConverterViewService viewService = ApplicationManager.getApplication().getService(DateTimeConverterViewService.class);
+
         viewService.initComponent();
         viewService.getModel().addRow(DateTimeUtil.getCurrentGasDayMs());
 
